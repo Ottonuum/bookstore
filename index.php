e2d1b334a9764ba4386c587ef9293dbc9a25a67b
Ottonuum <Nuumotto1@gmail.com>
2023-10-19 13:08:01 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$pdo = new PDO($dsn, $user, $pass, $options);
+
+
+$stmt = pdo->query('SELECT * from books')
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+    <?php
+    while ($row = $stmt->fetch()) {
+?>
+    <li><?=$row['title'];?></li>   
+
+<?php
+}
+?>
+</ul>
+
+</body>
+</html>
